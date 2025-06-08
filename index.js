ab6b7fb23d7123c1545e59607f51044b85ef2517
Prakash <prakashwijesinghanew@gmail.com>
2025-06-08 18:48:02 +0530

    feat: implement login route with JWT token generation

--- index.js
+++ index.js
@@ -8,6 +8,24 @@ app.get('/', (req, res) => {
     res.json( {msg:"Hello World!"} )
 })
 
+app.post("/login",(req,res)=>{
+
+    //request -->database-->true
+    //generate token
+    //sing userdata
+    var user = { id:1,username:"prakash",age:23};
+
+    jwt.sign( {user:userdata},"secretkey",(err,token) => {
+        if(err) {
+            res.json({error:err})
+        }else{
+            res.json({token:token})
+        }
+
+
+
+})
+
 app.listen(4000, () => {
     console.log('app is listening for port 4000')
 })
